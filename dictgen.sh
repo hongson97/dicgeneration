b46894613a4143504419db45248d8a696b01604f
hongson97 <hongson.uit@gmail.com>
2023-11-20 19:34:56 +0700

    update

--- dictgen.sh
+++ dictgen.sh
@@ -1,26 +1,35 @@
 #install tool
 echo "Install dependency tools:"
+go install github.com/jaeles-project/gospider@latest
 go install github.com/hakluke/hakrawler@latest
 go install github.com/tomnomnom/waybackurls@latest
 
 echo "Done"
 
 if [ -d "output" ]; then
-    echo "$DIRECTORY does exist."
+    echo "[+] Output folder is exist."
 else
     mkdir "output"
 fi
 
 if [ -d "output/js" ]; then
-    echo "$DIRECTORY does exist."
+    echo "[+] js folder is exist."
 else
-    mkdir "output/ls"
+    mkdir "output/js"
 fi
+start=$(date +%s)
 
-while read line; do
+while read domain; do
 # reading each line
-$output = "output/js" + $line
-echo $line | hakrawler >> $output
-echo $line | waybackurls >> $output
+clean_domain=`echo $domain | sed -E 's/^\s*.*:\/\///g'`
+outputfolder=output/js/$clean_domain
+mkdir $outputfolder
+echo $domain | gospider -q >> $outputfolder/gospider.txt
+echo $domain | waybackurls >> $outputfolder/waybackurls.txt
+cat $outputfolder/waybackurls.txt | unfurl pfolder -u > directory.txt
+cat $outputfolder/waybackurls.txt | unfurl pfile -u > filename.txt
+#echo $output
 n=$((n+1))
 done < $1
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
